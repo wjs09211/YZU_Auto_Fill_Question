6ae01546cd4c7d6d81770cd48cb3aec49465fa94
Sleep <wjs09211@gmail.com>
2015-10-16 21:38:58 +0800

    first

---
+++ YZU_Auto_Fill_Question/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace YZU_Auto_Fill_Question
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(117, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(263, 36);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(117, 25);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(445, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "登入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(-3, 110);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(641, 139);
+            this.listBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(474, 256);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "自動填寫";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "你的課程問卷:";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(-3, 291);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(641, 124);
+            this.listBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "帳號";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(207, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "密碼";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 261);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 19);
+            this.radioButton1.TabIndex = 9;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "非常同意";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(106, 261);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(58, 19);
+            this.radioButton2.TabIndex = 10;
+            this.radioButton2.Text = "同意";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(170, 261);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(88, 19);
+            this.radioButton3.TabIndex = 11;
+            this.radioButton3.Text = "尚可同意";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(263, 261);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(73, 19);
+            this.radioButton4.TabIndex = 12;
+            this.radioButton4.Text = "不同意";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(342, 261);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(103, 19);
+            this.radioButton5.TabIndex = 13;
+            this.radioButton5.Text = "非常不同意";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(639, 416);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "YZU_Auto_Fill_Question by Sleep";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+    }
+}
+
---
+++ YZU_Auto_Fill_Question/Form1.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Web;
+using System.Net;
+using System.IO;
+using System.Web.SessionState;
+namespace YZU_Auto_Fill_Question
+{
+    public partial class Form1 : Form
+    {
+        CookieContainer cookieContainer = new CookieContainer();
+        List<string> classUrl = new List<string>();
+        string html; //存放當前網頁內容
+        string SessionID;
+        bool click = true;
+        bool checkLogin = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox2.Items.Add("歡迎使用 YZU自動填寫課程問卷");
+        }
+        private void HttpGet(string url, bool cookie)
+        {
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.KeepAlive = true;
+                ///UserAgent 使用者的裝置和瀏覽器
+                request.UserAgent = "Mozilla/5.0 (MSIE 9.0; Windows NT 6.1; Trident/5.0)";
+                request.Accept = "application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, */*";
+                request.Headers.Add("Accept-Encoding", "gzip-deflate");
+                request.Headers.Add("Cache-Control", "no-cache");
+                request.Headers.Add("Accept-Language", "zh-TW");
+                if (cookie)
+                    request.CookieContainer = cookieContainer;
+                ///傳回的資料
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    StreamReader sr = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                    html = sr.ReadToEnd();
+                }
+            }
+            catch
+            {
+                listBox1.Items.Add("HttpGet error 程式錯誤");
+            }
+        }
+        private void HttpPost(string url, string param)
+        {
+            byte[] bs = Encoding.ASCII.GetBytes(param);
+            //requset
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "POST";
+                request.KeepAlive = true;
+                ///UserAgent 使用者的裝置和瀏覽器
+                request.UserAgent = "Mozilla/5.0 (MSIE 9.0; Windows NT 6.1; Trident/5.0)";
+                request.Accept = "application/x-ms-application, image/jpeg, application/xaml+xml, image/gif, image/pjpeg, application/x-ms-xbap, */*";
+                request.Headers.Add("Accept-Encoding", "gzip-deflate");
+                request.Headers.Add("Cache-Control", "no-cache");
+                request.Headers.Add("Accept-Language", "zh-TW");
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = bs.Length;
+                request.CookieContainer = cookieContainer;
+                using (Stream reqStream = request.GetRequestStream())
+                {
+                    reqStream.Write(bs, 0, bs.Length);
+                }
+                ///傳回的資料
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    StreamReader sr = new StreamReader(response.GetResponseStream(), System.Text.Encoding.GetEncoding("utf-8"));
+                    html = sr.ReadToEnd();                   
+                }
+            }
+            catch
+            {
+                listBox1.Items.Add("post error 程式錯誤");
+            }
+        }
+        private string getInfo(string s)
+        {         
+            int i = html.IndexOf(s) + s.Length;
+            int j = html.IndexOf("\"", i);
+            string info = html.Substring(i, j - i);
+            return info;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            login();
+            if (checkLogin == true)
+            {
+                HttpGet("https://portalx.yzu.edu.tw/PortalSocialVB/FMain/DefaultPage.aspx?Menu=Default", true);//portal 首頁
+                                                                                                               //模擬封包操作 到填問卷的頁面
+                HttpGet("https://portalx.yzu.edu.tw/PortalSocialVB/FMain/ClickMenuLog.aspx?type=App_&SysCode=A08", true);
+                HttpGet("https://portalx.yzu.edu.tw/PortalSocialVB/IFrameSub.aspx", true);
+                //取得SessionID
+                string str = "id='SessionID' name='SessionID' type='hidden' value='";
+                int start = html.IndexOf(str) + str.Length;
+                int end = html.IndexOf("'", start);
+                SessionID = html.Substring(start, end - start);
+                //
+                string formatString =
+                    "Account={0}&SessionID={1}&LangVersion=TW&Y=&M=&CosID=&CosClass=&UseType=STD";
+                string param =
+                         string.Format(formatString, textBox1.Text.ToString(), SessionID);
+                HttpPost("https://portal.yzu.edu.tw/NewSurvey/NewLogin.aspx", param);
+                HttpGet(" https://portal.yzu.edu.tw/NewSurvey/std/F01_Questionnaire.aspx", true);
+                getClass();
+                if (classUrl.Count != 0)
+                {
+                    listBox2.Items.Add("請選擇問卷評價，確定請按自動填寫");
+                }
+                else
+                {
+                    listBox2.Items.Add("無問卷可填寫");
+                    click = false;
+                }
+            }
+        }
+        private void getClass()
+        {
+            int start = 0;
+            string s = "</a></td><td><a href=\"";
+            string s2 = "target=\"_self\">";
+            while (true)
+            {
+                int i = html.IndexOf(s, start);
+                if (i == -1) break;
+                int j = html.IndexOf("\"", i + s.Length);
+                classUrl.Add(html.Substring(i + s.Length, j - i - s.Length).Replace("amp;", ""));
+                start = j;
+
+                i = html.IndexOf(s2, start);
+                if (i == -1) break;
+                j = html.IndexOf("<", i + s2.Length);
+                listBox1.Items.Add(html.Substring(i + s2.Length, j - i - s2.Length));
+                start = j + 3;
+            }
+
+        }
+        private void login()
+        {
+            HttpGet("https://portalx.yzu.edu.tw/PortalSocialVB/Login.aspx", false);
+            string viewState = getInfo("id=\"__VIEWSTATE\" value=\"");
+            string eventValidation = getInfo("id=\"__EVENTVALIDATION\" value=\"");
+            string submitButton = "登入";
+            //編碼一下~
+            viewState = HttpUtility.UrlEncode(viewState);
+            eventValidation = HttpUtility.UrlEncode(eventValidation);
+            submitButton = HttpUtility.UrlEncode(submitButton);
+            //POST資料
+            string formatString =
+                "__VIEWSTATE={0}&__EVENTVALIDATION={1}&Txt_UserID={2}&Txt_Password={3}&ibnSubmit={4}";
+            string param =
+                     string.Format(formatString, viewState, eventValidation, textBox1.Text.ToString(), textBox2.Text.ToString(), submitButton);
+            HttpPost("https://portalx.yzu.edu.tw/PortalSocialVB/Login.aspx", param);
+            if (html.IndexOf("DefaultPage.aspx") != -1)
+            {
+                listBox2.Items.Add("登入成功");
+                checkLogin = true;
+            }
+            else
+            {
+                listBox2.Items.Add("登入失敗");
+                checkLogin = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (click)
+            {
+                string value = "1";
+                if (radioButton1.Checked == true) value = "1";
+                else if (radioButton2.Checked == true) value = "2";
+                else if (radioButton3.Checked == true) value = "3";
+                else if (radioButton4.Checked == true) value = "4";
+                else if (radioButton5.Checked == true) value = "5";
+                for (int k = 0; k < classUrl.Count; k++)
+                {
+                    HttpGet("https://portal.yzu.edu.tw/NewSurvey/std/" + classUrl[k], true);
+                    string viewState = getInfo("id=\"__VIEWSTATE\" value=\"");
+                    string eventValidation = getInfo("id=\"__EVENTVALIDATION\" value=\"");
+                    string submitButton = "完成";
+                    //編碼一下~
+                    viewState = HttpUtility.UrlEncode(viewState);
+                    eventValidation = HttpUtility.UrlEncode(eventValidation);
+                    submitButton = HttpUtility.UrlEncode(submitButton);
+                    //POST資料
+                    string formatString =
+                        "__VIEWSTATE={0}&__EVENTVALIDATION={1}&btOK={2}";
+                    string param =
+                             string.Format(formatString, viewState, eventValidation, submitButton);
+                    //取得問卷內容
+                    string s = "valign=\"top\"><table id=\"";
+                    int start = 0;
+                    while (true)
+                    {
+                        int i = html.IndexOf(s, start);
+                        if (i == -1) break;
+                        int j = html.IndexOf("\"", i + s.Length);
+                        param += ("&" + html.Substring(i + s.Length, j - i - s.Length) + "=" + value);
+                        start = j;
+                    }
+                    HttpPost("https://portal.yzu.edu.tw/NewSurvey/std/" + classUrl[k], param);
+                }
+                listBox2.Items.Add("問卷已填完，請上網確認");
+                click = false;
+            }
+            else
+            {
+                if (classUrl.Count == 0)               
+                    listBox2.Items.Add("無問卷可填寫");                
+                else
+                    listBox2.Items.Add("已填完，請勿重複點擊");
+            }
+        }
+    }
+}
